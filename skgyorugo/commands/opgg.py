803d1713af95a6ab1f6882eef71339f2946b5e81
Georgios Atheridis <atheridis@tutamail.com>
2022-05-06 13:25:25 +0000

    fixed bug for opgg command

--- skgyorugo/commands/opgg.py
+++ skgyorugo/commands/opgg.py
@@ -37,25 +37,15 @@ def get_twitch_user_data(twitch_id: int):
 
 def main(bot: Bot, message: Message):
     try:
-        replied_message = message.tags["reply-parent-msg-body"]
-    except KeyError:
-        replied_message = None
-    if replied_message:
-        msg = replied_message
-        replied_msg_id = message.tags["reply-parent-msg-id"]
-    else:
-        msg = ' '.join(message.value.split(' ')[1:])
-        replied_msg_id = None
-
-    try:
         twitch_user = message.value.split(' ')[1]
     except IndexError:
+        twitch_user = message.tags.get("reply-parent-display-name", message.channel)
         twitch_id = message.tags.get("reply-parent-user-id", ttv_api.users.get_users(user_logins=[message.channel]))
     else:
         twitch_id = ttv_api.users.get_users(user_logins=[twitch_user])
 
     if not twitch_id:
-        logger.warning(f"There was an issue getting twitch data for user {twitch_id}; message was: {message.value}")
+        logger.warning(f"There was an issue getting twitch data for user {twitch_id}; message id was: {message.tags['id']}")
         smart_privmsg.send(bot, message, "Couldn't retrieve data", reply=message.tags["id"])
         return
 
@@ -76,7 +66,7 @@ def main(bot: Bot, message: Message):
     fetched = c.fetchall()
 
     if not fetched:
-        smart_privmsg.send(bot, message, f"No summoners added for that twitch user", reply=message.tags["id"])
+        smart_privmsg.send(bot, message, f"No summoners added for {twitch_user}", reply=message.tags["id"])
         return
     
     summoner_names = []
@@ -88,7 +78,7 @@ def main(bot: Bot, message: Message):
         if info:
             break
     else:
-        smart_privmsg.send(bot, message, f"{{channel}} is currently not in game. These are all of his summoners: {summoner_names}", reply=message.tags["id"])
+        smart_privmsg.send(bot, message, f"{twitch_user} is currently not in game. These are all of their summoners: {summoner_names}", reply=message.tags["id"])
         return
 
-    smart_privmsg.send(bot, message, f"{{channel}} is currently playing a game on: {summoner_names[-1]}", reply=message.tags["id"])
+    smart_privmsg.send(bot, message, f"{twitch_user} is currently playing a game on: {summoner_names[-1]}", reply=message.tags["id"])
